a280460fc196543c4d4f076648fdf08fa9391a24
oginofumihiro <fumi1127ogino@gmail.com>
2021-03-16 13:12:24 +0000

    [Finish] 4-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '38560dc751c28747c64be660af92f6ed784c972d2c9328afb8e23db3d8b844477e8a1c94f2ee3645158280089b1714d2dd171d9e751e2e8f5b2f86d1603ceb22'
